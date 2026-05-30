5fc19837cb95623a63ad65833c45281737af5009
saahithya-01 <bairapogusdraj@gmail.com>
2026-05-30 10:08:51 +0530

    update

--- backend/models/User.js
+++ backend/models/User.js
@@ -2,21 +2,32 @@ const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
   name: String,
+
   email: {
     type: String,
     unique: true
   },
+
   password: String,
+
   role: {
     type: String,
     default: 'user'
   },
+
   verified: {
     type: Boolean,
     default: false
   },
+
   verificationToken: String,
-  resetToken: String
-}, { timestamps: true });
+
+  resetToken: String,
+
+  resetTokenExpiry: Date
+
+}, {
+  timestamps: true
+});
 
 module.exports = mongoose.model('User', userSchema);
